d600bf9a6c9c4fb55376d2420d8bd84a95a0ca86
David <anddagger@gmail.com>
2022-08-27 18:58:52 +0000

    added FastAPI

--- app/copykitt_api.py
+++ app/copykitt_api.py
@@ -1,34 +1,46 @@
-# from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
 from copykitt import generate_branding_snippet, generate_keywords
-# from mangum import Mangum
-# from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
 
-# app = FastAPI()
-# handler = Mangum(app)
+app = FastAPI()
+handler = Mangum(app)
 MAX_INPUT_LENGTH = 32
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# @app.get("/generate_snippet")
+
+@app.get("/generate_snippet")
 async def generate_snippet_api(prompt: str):
-  return
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    return {"snippet": snippet, "keywords": []}
 
 
-# @app.get("/generate_keywords")
+@app.get("/generate_keywords")
 async def generate_keywords_api(prompt: str):
-  return
+    validate_input_length(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": None, "keywords": keywords}
 
 
-# @app.get("/generate_snippet_and_keywords")
+@app.get("/generate_snippet_and_keywords")
 async def generate_keywords_api(prompt: str):
-  return
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": snippet, "keywords": keywords}
 
 
 def validate_input_length(prompt: str):
-  pass
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Input length is too long. Must be under {MAX_INPUT_LENGTH} characters.",
+        )
